6ad35cf6b26cdadbd881ec964305176d3320cc98
Blair Jackson <blairjackson@Blairs-MacBook-Pro.local>
2018-11-30 15:35:40 +0000

    added server.js to run on heroku

--- src/server.js
+++ src/server.js
@@ -3,13 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const dist = '/dist/';
-const index = '/dist/index.html';
-
-app.use(express.static(`__dirname${dist}`));
+app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, (req, res) => {
-  res.sendfile(`__dirname${index}`);
+  res.sendfile(__dirname + '/dist/index.html')
 });
 
 app.listen(port);
-// console.log('server started on ' + port);
+console.log('server started on ' + port);
